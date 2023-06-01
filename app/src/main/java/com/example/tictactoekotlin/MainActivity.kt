c056c86fc5e5fc6496c681713e6b1298e1bba4cd
Tanmoy Sharma <tonmoytonu55@gmail.com>
2023-06-01 18:41:21 +0600

    design ready

---
+++ app/src/main/java/com/example/tictactoekotlin/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.tictactoekotlin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.tictactoekotlin.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding.logoimg.translationY = -1000f
+        binding.gamename.translationY = 1000f
+
+        binding.logoimg.animate().translationY( 0f).duration = 2000
+        binding.gamename.animate().translationY( 0f).duration = 2000
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            var intent= Intent(this,StartActivity::class.java)
+            startActivity(intent)
+        },3000)
+
+    }
+}
---
+++ app/src/main/java/com/example/tictactoekotlin/StartActivity.kt
@@ -0,0 +1,15 @@
+package com.example.tictactoekotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+
+
+
+    }
+}
