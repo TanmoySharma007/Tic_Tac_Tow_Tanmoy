3db58e65d57dedae9517f4d44bd67199467bf180
Tanmoy Sharma <tonmoytonu55@gmail.com>
2023-06-04 17:21:00 +0600

    buttons turned into AppCompat Buttons

--- app/src/main/java/com/example/tictactoekotlin/GameActivity.kt
+++ app/src/main/java/com/example/tictactoekotlin/GameActivity.kt
@@ -1,11 +1,13 @@
 package com.example.tictactoekotlin
 
-import android.app.Activity
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import androidx.cardview.widget.CardView
 import com.example.tictactoekotlin.databinding.ActivityGameBinding
 import com.example.tictactoekotlin.databinding.ActivityMainBinding
@@ -29,20 +31,64 @@ class GameActivity : AppCompatActivity() {
              startActivity( Intent(this,StartActivity::class.java))
          }
 
-            fun buttonclick( view : View){
-
-                val buttonSelected = view as Button
-
-
-            }
     }
 
+   fun buclick ( view : View){
 
+         var cellId = 0
+        val buSelected = view as Button
 
+            when (buSelected.id){
+                R.id.button1 ->
+                    cellId = 1
+                R.id.button2 ->
+                    cellId = 2
+                R.id.button3 ->
+                    cellId = 3
+                R.id.button4 ->
+                    cellId = 4
+                R.id.button5 ->
+                    cellId = 5
+                R.id.button6 ->
+                    cellId = 6
+                R.id.button7 ->
+                    cellId = 7
+                R.id.button8 ->
+                    cellId = 8
+                R.id.button9 ->
+                    cellId = 9
 
+            }
+                    Log.d("buclicked: ",buSelected.id.toString())
+                    Log.d("cellid: ",cellId.toString())
 
+       gameplay(cellId,buSelected) // Gameplay Function is called in the viewClick Function
 
+    }
 
+    var activePlayer = 1
+
+    var player1 = ArrayList<Int>()
+    var player2 = ArrayList<Int>()
+
+     fun gameplay (cellid:Int ,buSelected: Button) {
+
+         if (activePlayer==1) {
+             buSelected.text= "X"
+             buSelected.setBackgroundResource(R.drawable.circular_button)
+             player1.add(cellid)
+             activePlayer = 2
+
+         } else{
+             buSelected.text= "O"
+
+             buSelected.setBackgroundResource(R.drawable.circular_button)
+             player1.add(cellid)
+             activePlayer = 1
+         }
+
+         buSelected.isEnabled = false
+     }
 
 
 }
